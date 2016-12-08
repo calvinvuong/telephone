f6ae0503dddc0cf4555b5c4c27c59007ba20d979
Calvin Vuong <cvuong@stuy.edu>
2016-12-08 15:28:31 -0500

    initial commit

---
+++ control.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <string.h>
+
+ // declaration required on linux
+union semun {  
+  int val;
+  struct semid_ds *buf;
+  unsigned short *array; 
+  struct seminfo *__buf;
+};
+
+int main(int argc, char *argv[]) {
+  
+  int shmid;
+  int line_size;
+  int semid;
+  int key = ftok( "control.c", 1337 );
+  int sc;
+  union semun su;
+
+  // creates semaphore and set value to arg2
+  if ( strcmp( argv[1], "-c" ) == 0 ) {
+    // make shared memory; set to 0
+    shmid = shmget( key, 8, IPC_CREAT | IPC_EXCL );
+    line_size = shmat( shmid, 0, 0 );
+    *line_size = 0;
+    
+    // make semaphore
+    semid = semget( key, 1, IPC_CREAT | IPC_EXCL | 0644 );
+    if ( semid >= 0 ) {
+      su.val = ato]);
+      sc = semctl( semid, 0, SETVAL, su );
+
+    }
+    else
+      printf("semaphore already exists\n");
+  }
+
+  // prints out value of the semaphore
+  else if ( strcmp( argv[1], "-v" ) == 0 ) {
+    semid = semget( key, 1, 0 );
+    sc = semctl( semid, 0, GETVAL );
+    printf("semaphore value: %d\n", sc);
+  }
+  
+  // removes the semaphore
+  else if ( strcmp( argv[1], "-r" ) == 0 ) {
+    semid = semget( key, 1, 0 );
+    sc = semctl( semid, 0, IPC_RMID );
+    printf("semaphore removed: %d\n", sc);
+  }
+
+  return 0;
+}
